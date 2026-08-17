0425252d702dd21ee3c5f3a12595f0a4cfb7b82c
ptr47 <70442277+ptr47@users.noreply.github.com>
2026-08-17 03:00:29 +0200

    add solar panel efficiency based on time of day

--- src/main/java/org/ptr47/tfmg_stellaris_compat/generators/SolarPanelBlockEntity.java
+++ src/main/java/org/ptr47/tfmg_stellaris_compat/generators/SolarPanelBlockEntity.java
@@ -1,9 +1,7 @@
 package org.ptr47.tfmg_stellaris_compat.generators;
 
 import com.drmangotea.tfmg.content.electricity.base.ElectricBlockEntity;
-import com.simibubi.create.foundation.block.IBE;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.ptr47.tfmg_stellaris_compat.config.TFMGStellarisCompatConfigs;
@@ -12,27 +10,57 @@ public class SolarPanelBlockEntity extends ElectricBlockEntity {
 
     public SolarPanelBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
+
+        setLazyTickRate(10);
     }
 
     @Override
     public int voltageGeneration() {
-        if (level.isClientSide || level.isNight()) {
+        if (solarEfficiency() <= 0.0f) {
             return 0;
         }
+
         return TFMGStellarisCompatConfigs.common().solarPanelConfig.voltage.get();
     }
 
     @Override
     public void lazyTick() {
         super.lazyTick();
-        updateNetwork();
+        updateNextTick();
     }
 
     @Override
     public int powerGeneration() {
-        if (level.isClientSide || level.isNight() || !level.canSeeSkyFromBelowWater(worldPosition.above())) {
-            return 0;
+        int basePower = TFMGStellarisCompatConfigs.common().solarPanelConfig.power.get();
+
+        return Math.round(basePower * solarEfficiency());
+    }
+
+    @Override
+    public int getMaxVoltage() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxCurrent() {
+        return 0;
+    }
+
+    private float solarEfficiency() {
+        if (level == null || !level.canSeeSky(worldPosition.above()) || level.isThundering()) {
+            return 0.0f;
         }
-        return TFMGStellarisCompatConfigs.common().solarPanelConfig.power.get();
+
+        long time = level.getDayTime() % 24000;
+        if (time < 12000) {
+            return 1.0f;
+        }
+        if (time < 13000) {
+            return 1.0f - (time - 12000) / 1000.0f;
+        }
+        if (time < 23000) {
+            return 0.0f;
+        }
+        return (time - 23000) / 1000.0f;
     }
 }
